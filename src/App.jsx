06a0ea5b4e2db7c060b1ed6ce64c0e0c0480d0f8
ElAmineMohamed <bouzidaminem@gmail.com>
2023-10-24 22:54:33 +0100

    add Card & Button

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="card-item">
+            <div className="left-items">
+              <span className="3point">...</span>
+            </div>
+            <div className="right-items">
+              <p>Lorem, ipsum dolor sit amet consectetur adi</p>
+            </div>
+          </div>
+          <div className="card-item">
+            <div className="left-items">
+              <span className="spoint">...</span>
+            </div>
+            <div className="right-items">
+              <p>Lorem, ipsum dolor sit amet consectetur adi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
